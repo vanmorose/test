4c1b90442a850844197e8e5464b596eb08f6e9ef
Ivan_Morozov <ivanmorose@gmail.com>
2022-08-03 18:58:10 +0300

    Новый тест

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
-print('Hello Git!')
-print(
-    'How are you?'
-)
+
+
+class Cats:
+    test_voice = 'Miy'
+
+    @classmethod
+    def validate(cls, *args):
+        return cls.test_voice
+
+    def __init__(self, name: str, len_hair: int, tone_voice: str) -> None:
+        self.name = name
+        self.len_hair = len_hair
+        self.tone_voice = tone_voice
+
+    def __del__(self):
+        print('Удаление: ' + str(self))
+
+    def get_voice(tone_voice) -> str:
+        return tone_voice
+
+    def name_cat(self):
+        return self.name
+
+
+ct = Cats("Murka", 20, "Mey")
+Cats.validate(2)
+res = ct.name_cat()
+print(res)
+print(ct.__dict__)
